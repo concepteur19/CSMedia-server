d77c23ca1abb2a455a60c04489a4ae7546097526
concepteur19 <zobel.tchomgui@gmail.com>
2023-01-26 23:01:37 +0100

    CSMedia server first commit

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
+import User from "../models/User.js"
+
+/* Register user */
+export const register = async (req, res) => {
+  
+}
---
+++ models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+      unique: true,
+    },
+    passwod: {
+      type: String,
+      require: true,
+      min: 8,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impression: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+export default User;
